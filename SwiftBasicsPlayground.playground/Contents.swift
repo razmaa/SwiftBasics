be5d7a7c0d311b8fb7801194135c008a8f7ff9b1
razmaa <145837511+razmaa@users.noreply.github.com>
2025-05-24 07:35:27 +0400

    is Balanced Parentheses func

--- SwiftBasicsPlayground.playground/Contents.swift
+++ SwiftBasicsPlayground.playground/Contents.swift
@@ -8,9 +8,36 @@ public func isPalindrome(input: String) -> Bool {
     return cleanedInput == String(cleanedInput.reversed())
 }
 
+print("Answers for palindromes:")
 print(isPalindrome(input: "racecar"))
 print(isPalindrome(input: "hello"))
 print(isPalindrome(input: "A man, named Nika, will be great developer::q"))
 print(isPalindrome(input: " "))
 print(isPalindrome(input: "a"))
 print(isPalindrome(input: "No 'x' in Nixon"))
+
+print("")
+
+public func isBalancedParentheses(input: String) -> Bool {
+    var count = 0
+    
+    for char in input {
+        if char == "(" {
+            count += 1
+        } else if char == ")" {
+            count -= 1
+        }
+        
+        if count < 0 {
+            return false
+        }
+    }
+    
+    return count == 0
+}
+
+print("Answers for balanced parentheses:")
+print(isBalancedParentheses(input: "()"))
+print(isBalancedParentheses(input: "(())"))
+print(isBalancedParentheses(input: "(()))"))
+print(isBalancedParentheses(input: "())("))
